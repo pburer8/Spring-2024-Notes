00eda15cf024f782f793c5958d06ed6f4604e221
pburer <marie-burer@uiowa.edu>
2024-03-26 09:14:35 -0500

    h

--- Operating Systems/Homework/hw3/task1/page.c
+++ Operating Systems/Homework/hw3/task1/page.c
@@ -10,9 +10,9 @@ struct Page {
 };
 
 struct DLL {
-    struct Page page;
-    struct DLL prev;
-    struct DLL next;
+    struct Page* data;
+    struct DLL* prev;
+    struct DLL* next;
 };
 
 void FIFO() {
@@ -36,7 +36,12 @@ void FIFO() {
 
     while (fgets(line, 25, fptr)) {
         int id = atoi(strtok(line, " "));
-        char* read = atoi(strtok(NULL, " "));
+        char read = strtok(NULL, " ")[0];
+        int r = 0;
+        if (read == 'w') {
+            r = 1;
+        }
+        
         if (in[id] == 1) {
             continue;
         } else {
@@ -48,40 +53,107 @@ void FIFO() {
             in[id] = 1;
 
 
-            struct Page new_page = {.id=id, .read=read};
+            struct Page new_page = {.id=id, .read=r};
             frames[ptr] = new_page;
             ptr = (ptr + 1) % MEM_SIZE;
         }
     }
 
-    printf("Number of faults with FIFO model: %d", faults);
+    printf("Number of faults with FIFO model: %d\n", faults);
+}
+
+struct DLL* create_node(struct Page* data) {
+    struct DLL* new_node = (struct DLL*) malloc(sizeof(struct DLL));
+    new_node->data = data;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+    return new_node;
+}
+
+void append(struct DLL* head, struct Page* data) {
+    struct DLL* new_node = create_node(data);
+
+    while (head->next != NULL) {
+        head = head->next;
+    }
+
+    head->next = new_node;
+    new_node->prev = head;
 }
 
 void LRU() {
-    struct DLL head;
+    printf("here\n");
+    struct Page d = {.id = -1, .read = -1};
+    struct DLL* head = create_node(&d);
     int faults = 0;
     int in[100];
-    for (int i = 0; i < MEM_SIZE; i++) {
-        struct Page fill = {.id = -1, .read = -1};
-        
+
+    for (int i = 0; i < 19; i++) {
+        struct Page fill = {.id=-1, .read=-1};
+        append(head, &fill);
     }
+
     for (int i = 0; i < 100; i++) {
         in[i] = 0;
+    }
+    struct DLL* tail = head;
+    while (tail->next != NULL) {
+        tail = tail->next;
     }
 
     FILE* fptr = fopen("page_references.txt", "r");
 
     char line[25];
-    
+    printf("here2\n");
     while (fgets(line, 25, fptr)) {
         int id = atoi(strtok(line, " "));
-        int read = atoi(strtok(NULL, " "));
-        if (in[id] == 1) {
+        char read = strtok(NULL, " ")[0];
+        int r = 0;
+        if (read == 'w') {
+            r = 1;
         }
+        if (in[id] == 1) {
+            struct DLL* ptr = head;
+            while(ptr->data->id != id) {
+                ptr = ptr->next;
+            }
+
+            if (ptr->prev != NULL) {
+                ptr->prev->next = ptr->next;
+            }
+            if (ptr->next != NULL) {
+                ptr->next->prev = ptr->prev;
+            } else {
+                tail = ptr->prev;
+            }
+            ptr->prev = NULL;
+            ptr->next = head;
+            head->prev = ptr;
+            head = ptr;
+        } else {
+            faults++;
+
+            tail = tail->prev;
+            struct Page p = *tail->next->data;
+            in[p.id] = 0;
+            tail->next = NULL;
+
+            struct Page np = {.id=id, .read=r};
+            struct DLL* new = create_node(&np);
+            in[id] = 1;
+
+            new->next = head;
+            head->prev = new;
+            head = new;
+        }
+        printf("here3\n");
     }
+
+    printf("Number of faults with LRU model: %d\n", faults);
 }
 
 int main() {
-    FIFO();
+    //FIFO();
+    LRU();
     return 0;
 }
