15f88d10fb7361610fbe8f6f1fbd09d7656e60c2
pburer <marie-burer@uiowa.edu>
2024-03-27 21:28:30 -0500

    h

--- Operating Systems/Homework/hw3/task1/page.c
+++ Operating Systems/Homework/hw3/task1/page.c
@@ -4,16 +4,96 @@
 #include<string.h>
 
 const int MEM_SIZE = 20;
+
 struct Page {
     int id;
     int read; //read = 0, write = 1
 };
 
-struct DLL {
+struct node {
     struct Page* data;
-    struct DLL* prev;
-    struct DLL* next;
+    struct node* prev;
+    struct node* next;
 };
+
+struct node* create_node(struct Page* data) {
+    struct node* n = malloc(sizeof(struct node));
+
+    n->data = data;
+    n->prev = NULL;
+    n->next = NULL;
+
+    return n;
+}
+
+struct node* append(struct node* tail, struct Page* data) {
+    struct node* appendee = create_node(data);
+
+    tail->next = appendee;
+    appendee->prev = tail;
+
+    return appendee;
+}
+
+void use(struct node** head, struct node** tail, struct node** node) {
+
+    if (!(*node)->prev) {
+        (*head) = (*node);
+    } else if (!(*node)->next) {
+        (*tail)->prev->next = NULL;
+        (*tail) = (*tail)->prev;
+
+        (*head)->prev = (*node);
+        (*node)->prev->next = (*node)->next;
+        (*node)->next = (*head);
+        (*node)->prev = NULL;      
+
+        (*head) = (*node);  
+    } else {
+        (*head)->prev = (*node);
+        (*node)->prev->next = (*node)->next;
+        (*node)->next->prev = (*node)->prev;
+        (*node)->next = (*head);
+        (*node)->prev = NULL;
+        
+        (*head) = (*node);
+    }
+}
+
+void swap(struct node** head, struct node** tail, struct Page* data) {
+
+    struct node* n = create_node(data);
+
+    (*head)->prev = n;
+    n->next = (*head);
+    (*head) = n;
+
+    (*tail)->prev->next = NULL;
+    (*tail) = (*tail)->prev;
+}
+
+void print_list(struct node* head) {
+
+    while (head) {
+        struct Page p = *(head->data);
+        printf("Node #%d ", p.id);
+
+        if (p.read == 0) {
+            printf("(Read) ");
+        } else if (p.read == 1) {
+            printf("(Write) ");
+        } else {
+            printf("(None) ");
+        }
+    
+
+        head = head->next;
+        if (head) {
+            printf("<--> \n");
+        }
+    }
+    printf("\n");
+}
 
 void FIFO() {
     struct Page frames[MEM_SIZE];
@@ -34,6 +114,7 @@ void FIFO() {
 
     char line[25];
 
+    int page_request_count = 0;
     while (fgets(line, 25, fptr)) {
         int id = atoi(strtok(line, " "));
         char read = strtok(NULL, " ")[0];
@@ -42,9 +123,7 @@ void FIFO() {
             r = 1;
         }
         
-        if (in[id] == 1) {
-            continue;
-        } else {
+        if (in[id] != 1) {
             faults++;
             struct Page prev_page = frames[ptr];
             if (prev_page.id != -1) {
@@ -57,54 +136,40 @@ void FIFO() {
             frames[ptr] = new_page;
             ptr = (ptr + 1) % MEM_SIZE;
         }
+        page_request_count++;
     }
 
     printf("Number of faults with FIFO model: %d\n", faults);
-}
-
-struct DLL* create_node(struct Page* data) {
-    struct DLL* new_node = (struct DLL*) malloc(sizeof(struct DLL));
-    new_node->data = data;
-    new_node->prev = NULL;
-    new_node->next = NULL;
-    return new_node;
-}
-
-void append(struct DLL* head, struct Page* data) {
-    struct DLL* new_node = create_node(data);
-
-    while (head->next != NULL) {
-        head = head->next;
-    }
-
-    head->next = new_node;
-    new_node->prev = head;
+    float percent_rate = (float) faults / (float) page_request_count;
+    printf("Percentage rate: %.2f%%\n", percent_rate*100);
 }
 
 void LRU() {
-    printf("here\n");
-    struct Page d = {.id = -1, .read = -1};
-    struct DLL* head = create_node(&d);
+    struct Page filler = {.id = -1, .read = -1};
+    struct node* head = create_node(&filler);
+    
+    struct node* tail = head;
+
+    for (int i = 2; i <= 20; i++) {
+        struct Page* f = malloc(sizeof(struct Page));
+
+        f->id = i * -1;
+        f->read = -1;
+
+        tail = append(tail, f);
+    }
+
     int faults = 0;
     int in[100];
-
-    for (int i = 0; i < 19; i++) {
-        struct Page fill = {.id=-1, .read=-1};
-        append(head, &fill);
-    }
 
     for (int i = 0; i < 100; i++) {
         in[i] = 0;
     }
-    struct DLL* tail = head;
-    while (tail->next != NULL) {
-        tail = tail->next;
-    }
 
     FILE* fptr = fopen("page_references.txt", "r");
-
     char line[25];
-    printf("here2\n");
+
+    int page_request_count = 0;
     while (fgets(line, 25, fptr)) {
         int id = atoi(strtok(line, " "));
         char read = strtok(NULL, " ")[0];
@@ -112,48 +177,40 @@ void LRU() {
         if (read == 'w') {
             r = 1;
         }
+
         if (in[id] == 1) {
-            struct DLL* ptr = head;
-            while(ptr->data->id != id) {
-                ptr = ptr->next;
+            struct node* node = malloc(sizeof(struct node));
+            node = head;
+            while (node->data->id != id) {
+                node = node->next;
             }
 
-            if (ptr->prev != NULL) {
-                ptr->prev->next = ptr->next;
-            }
-            if (ptr->next != NULL) {
-                ptr->next->prev = ptr->prev;
-            } else {
-                tail = ptr->prev;
-            }
-            ptr->prev = NULL;
-            ptr->next = head;
-            head->prev = ptr;
-            head = ptr;
+            use(&head, &tail, &node);
         } else {
             faults++;
 
-            tail = tail->prev;
-            struct Page p = *tail->next->data;
-            in[p.id] = 0;
-            tail->next = NULL;
+            struct Page* p = malloc(sizeof(struct Page));
+            p->id = id;
+            p->read = r;
 
-            struct Page np = {.id=id, .read=r};
-            struct DLL* new = create_node(&np);
+            int prev_id = tail->data->id;
+
+            if (prev_id >= 0) {
+                in[prev_id] = 0;
+            }
             in[id] = 1;
 
-            new->next = head;
-            head->prev = new;
-            head = new;
+            swap(&head, &tail, p);
         }
-        printf("here3\n");
+        page_request_count++;
     }
-
     printf("Number of faults with LRU model: %d\n", faults);
+    float percent_rate = (float) faults / (float) page_request_count;
+    printf("Percent rate: %.2f%%\n", percent_rate*100);
 }
 
 int main() {
-    //FIFO();
+    FIFO();
     LRU();
     return 0;
 }
